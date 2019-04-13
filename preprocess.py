ec593714ece06506d9e159a3302f16351bc3951f
Taresh Sethi <taresh@Tareshs-MacBook-Air.local>
2019-04-13 17:01:01 +0200

    remove non images via threading

--- preprocess.py
+++ preprocess.py
@@ -1,26 +1,70 @@
 import ast
 import pickle
+from joblib import Parallel, delayed
+import os
 
 save_prefix = 'data/'
-save_filenames = ['total_preprocessed_small', 'subreddits_small', 'dataset_small']
+splitted_data_prefix = save_prefix + 'splitted/'
+SMALL = True
+save_filenames = ['total_preprocessed', 'subreddits', 'dataset']
 
-def preprocess(filename):
+def preprocess_job(filename):
+    print ('start job')
     to_save = [[],[],[]] # list of preprocessed_raw_lines, list of subreddit titles, list of posts
     with open (filename) as f:
-        counter = 0
         for line in f:
-            line_ = ast.literal_eval(line)
-            if '.jpg' in line_[2] or '.png' in line_[2]:
-                to_save[0].append(line)
-                to_save[1].append(line_[0])
-                to_save[2].append(line_[1])
-            if counter == 10000:
-                break
-            counter += 1
+            try:
+                line_ = ast.literal_eval(line)
+                if '.jpg' in line_[2] or '.png' in line_[2]:
+                    to_save[0].append(line)
+                    to_save[1].append(line_[0])
+                    to_save[2].append(line_[1])
+            except ValueError:
+                pass  # do nothing!
+
+    print ('finish job')
+
+    return to_save
+
+def preprocess(directory_of_splitted_files):
+    for r, d, f in os.walk(directory_of_splitted_files):
+        arg_instances = [splitted_data_prefix + s for s in f]
+        break
+
+    arg_instances = arg_instances[:8]
+    print (arg_instances)
+    
+    to_save = Parallel(n_jobs=-1, verbose=5, backend="threading")(
+        map(delayed(preprocess_job), arg_instances))
 
     for i, filename in enumerate(save_filenames):
         with open (save_prefix + filename + '.pkl', 'wb') as f:
             pickle.dump(to_save[i], f)
+
+
+# def preprocess(filename):
+#     to_save = [[],[],[]] # list of preprocessed_raw_lines, list of subreddit titles, list of posts
+#     with open (filename) as f:
+#         counter = 0
+#         for line in f:
+#             try:
+#                 line_ = ast.literal_eval(line)
+#                 if '.jpg' in line_[2] or '.png' in line_[2]:
+#                     to_save[0].append(line)
+#                     to_save[1].append(line_[0])
+#                     to_save[2].append(line_[1])
+#             except ValueError:
+#                 pass  # do nothing!
+#             if SMALL:
+#                 if counter == 10000000:
+#                     break
+#                 counter += 1
+
+#     for i, filename in enumerate(save_filenames):
+#         if SMALL:
+#             filename += '_small'
+#         with open (save_prefix + filename + '.pkl', 'wb') as f:
+#             pickle.dump(to_save[i], f)
 
 def load(filename='dataset'):
     with open (save_prefix + filename + '.pkl', 'rb') as f:
@@ -28,4 +72,4 @@ def load(filename='dataset'):
     return dataset
 
 if __name__ == '__main__':
-    preprocess('safe_links_all')
+    preprocess(save_prefix + 'splitted/')
