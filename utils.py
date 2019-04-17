d28ac7a921977a1a69707974a3a376c516cc4bdf
Taresh Sethi <taresh@Tareshs-MacBook-Air.local>
2019-04-17 21:57:45 +0200

    readme modification

--- utils.py
+++ utils.py
@@ -1,5 +1,23 @@
 from gensim.scripts.glove2word2vec import glove2word2vec
 import pickle
+import torch.utils.data as data
+
+class Dataset(data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, input_, output_):
+        'Initialization'
+        self.input_ = input_
+        self.output_ = output_
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.input_)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        x = self.input_[index]
+        y = self.output_[index]
+        return x, y
 
 def convert_glove_to_word2vec():
     glove_input_file = data_prefix + 'glove.6B.100d.txt'
